56736cbe4c4f58bbffeb0e40a9dbbf9d82d951e0
Mariam-maghraby <61349498+Mariam-maghraby@users.noreply.github.com>
2023-10-08 15:51:01 +0300

    fix: image preview

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,10 @@
 import "./App.css";
-import { InsertImagePlaceHolder } from "./components/ImageInserter";
-// import { ImagePreview } from "./components/ImageViewer";
+import { ImageViewer } from "./components/ImageViewer";
 
 function App() {
   return (
     <>
-      <InsertImagePlaceHolder />
-      {/* <ImagePreview
-        image={
-          "https://images.unsplash.com/photo-1508193638397-1c4234db14d8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80"
-        }
-        onLoad={function (): void {
-          console.log("onLoad");
-        }}
-      /> */}
+      <ImageViewer />
     </>
   );
 }
--- src/components/ImageViewer.tsx
+++ src/components/ImageViewer.tsx
@@ -1,20 +1,76 @@
-import { Box, Image } from "@mantine/core";
+import { useState } from "react";
+import { Text, Image, Box, Group, useMantineTheme } from "@mantine/core";
+import { IconPhoto, IconUpload, IconX } from "@tabler/icons-react";
+import { Dropzone, MIME_TYPES, FileWithPath } from "@mantine/dropzone";
 
-export interface ImagePreviewProps {
-  image: string;
-  onLoadImg: () => void;
-}
+export function ImageViewer() {
+  const theme = useMantineTheme();
+  const [files, setFiles] = useState<FileWithPath[]>([]);
 
-export function ImagePreview({ image: url, onLoadImg }: ImagePreviewProps) {
-  return (
-    <Box className="flex justify-center">
+  const previews = files.map((file, index) => {
+    const imageUrl = URL.createObjectURL(file);
+    return (
       <Image
-        classNames={{ imageWrapper: "flex justify-center" }}
-        width={"100%"}
-        src={url}
-        alt={"an uploaded image"}
-        imageProps={{ onLoad: onLoadImg }}
+        key={index}
+        src={imageUrl}
+        imageProps={{ onLoad: () => URL.revokeObjectURL(imageUrl) }}
       />
-    </Box>
-  );
+    );
+  });
+
+  if (files.length == 0) {
+    return (
+      <Dropzone
+        onDrop={setFiles}
+        onReject={(files) => console.log("rejected files", files)}
+        maxSize={3 * 1024 ** 2}
+        accept={[MIME_TYPES.png, MIME_TYPES.jpeg, MIME_TYPES.svg]}>
+        <Group
+          position="center"
+          spacing="xl"
+          style={{ minHeight: 220, pointerEvents: "none" }}>
+          <Dropzone.Accept>
+            <IconUpload
+              size={50}
+              stroke={1.5}
+              color={
+                theme.colors[theme.primaryColor][
+                  theme.colorScheme === "dark" ? 4 : 6
+                ]
+              }
+            />
+          </Dropzone.Accept>
+          <Dropzone.Reject>
+            <IconX
+              size={50}
+              stroke={1.5}
+              color={theme.colors.red[theme.colorScheme === "dark" ? 4 : 6]}
+            />
+          </Dropzone.Reject>
+          <Dropzone.Idle>
+            <IconPhoto size={50} stroke={1.5} />
+          </Dropzone.Idle>
+
+          <div>
+            <Text size="xl" inline>
+              Drag images here or click to select files
+            </Text>
+            <Text size="sm" className="text-gray-500" inline mt={7}>
+              Attach as many files as you like, each file should not exceed 5mb
+            </Text>
+          </div>
+        </Group>
+      </Dropzone>
+    );
+  }
+
+  if (files.length > 0) {
+    return (
+      <Box
+      //   className="flex justify-center"
+      >
+        {previews}
+      </Box>
+    );
+  }
 }
