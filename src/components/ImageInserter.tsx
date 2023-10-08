237b28d65e2e3fde7a16c5ac98f22841f1ad794f
Mariam-maghraby <61349498+Mariam-maghraby@users.noreply.github.com>
2023-10-08 12:25:43 +0300

    fix: accept images only of type jpeg, png & svg

--- src/components/ImageInserter.tsx
+++ src/components/ImageInserter.tsx
@@ -1,56 +1,68 @@
-import { Group, Text, useMantineTheme } from "@mantine/core"
-import { Dropzone, DropzoneProps, FileWithPath, IMAGE_MIME_TYPE } from "@mantine/dropzone"
-import { IconPhoto, IconUpload, IconX } from "@tabler/icons-react"
+import { Group, Text, useMantineTheme } from "@mantine/core";
+import {
+  Dropzone,
+  DropzoneProps,
+  FileWithPath,
+  MIME_TYPES,
+} from "@mantine/dropzone";
+import { IconPhoto, IconUpload, IconX } from "@tabler/icons-react";
 
 interface ImagePlaceHolderProps extends Partial<DropzoneProps> {
-    onUpload: (files: FileWithPath[]) => void
+  onUpload: (files: FileWithPath[]) => void;
 }
 
-export function InsertImagePlaceHolder({ onUpload, ...props }: ImagePlaceHolderProps) {
-    const theme = useMantineTheme()
+export function InsertImagePlaceHolder({
+  onUpload,
+  ...props
+}: ImagePlaceHolderProps) {
+  const theme = useMantineTheme();
 
-    const handleUpload = (files: FileWithPath[]) => {
-        onUpload(files)
-    }
+  const handleUpload = (files: FileWithPath[]) => {
+    onUpload(files);
+  };
 
-    return (
-        <Dropzone
-            onDrop={handleUpload}
-            onReject={(files) => console.log("rejected files", files)}
-            maxSize={3 * 1024 ** 2}
-            accept={IMAGE_MIME_TYPE}
-            {...props}
-        >
-            <Group position="center" spacing="xl" style={{ minHeight: 220, pointerEvents: "none" }}>
-                <Dropzone.Accept>
-                    <IconUpload
-                        size={50}
-                        stroke={1.5}
-                        color={
-                            theme.colors[theme.primaryColor][theme.colorScheme === "dark" ? 4 : 6]
-                        }
-                    />
-                </Dropzone.Accept>
-                <Dropzone.Reject>
-                    <IconX
-                        size={50}
-                        stroke={1.5}
-                        color={theme.colors.red[theme.colorScheme === "dark" ? 4 : 6]}
-                    />
-                </Dropzone.Reject>
-                <Dropzone.Idle>
-                    <IconPhoto size={50} stroke={1.5} />
-                </Dropzone.Idle>
+  return (
+    <Dropzone
+      onDrop={handleUpload}
+      onReject={(files) => console.log("rejected files", files)}
+      maxSize={3 * 1024 ** 2}
+      accept={[MIME_TYPES.png, MIME_TYPES.jpeg, MIME_TYPES.svg]}
+      {...props}>
+      <Group
+        position="center"
+        spacing="xl"
+        style={{ minHeight: 220, pointerEvents: "none" }}>
+        <Dropzone.Accept>
+          <IconUpload
+            size={50}
+            stroke={1.5}
+            color={
+              theme.colors[theme.primaryColor][
+                theme.colorScheme === "dark" ? 4 : 6
+              ]
+            }
+          />
+        </Dropzone.Accept>
+        <Dropzone.Reject>
+          <IconX
+            size={50}
+            stroke={1.5}
+            color={theme.colors.red[theme.colorScheme === "dark" ? 4 : 6]}
+          />
+        </Dropzone.Reject>
+        <Dropzone.Idle>
+          <IconPhoto size={50} stroke={1.5} />
+        </Dropzone.Idle>
 
-                <div>
-                    <Text size="xl" inline>
-                        Drag images here or click to select files
-                    </Text>
-                    <Text size="sm" className="text-gray-500" inline mt={7}>
-                        Attach as many files as you like, each file should not exceed 5mb
-                    </Text>
-                </div>
-            </Group>
-        </Dropzone>
-    )
+        <div>
+          <Text size="xl" inline>
+            Drag images here or click to select files
+          </Text>
+          <Text size="sm" className="text-gray-500" inline mt={7}>
+            Attach as many files as you like, each file should not exceed 5mb
+          </Text>
+        </div>
+      </Group>
+    </Dropzone>
+  );
 }
