cceb07618ddb56a6be8c47eda0bd47d5f984b889
Mariam-maghraby <61349498+Mariam-maghraby@users.noreply.github.com>
2023-10-18 17:14:13 +0300

    feat: enable mouse drawing on canvas

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 interface Image {
   imgSrc: string;
@@ -6,6 +6,10 @@ interface Image {
 
 const Canvas = (props: Image) => {
   const canvasRef = useRef(null);
+  const contextRef = useRef(null);
+
+  const [isDrawing, setIsDrawing] = useState(false);
+
   useEffect(() => {
     const canvas = canvasRef.current;
     const context = canvas.getContext("2d");
@@ -15,11 +19,59 @@ const Canvas = (props: Image) => {
     };
     img.src = props.imgSrc;
 
-    // context.fillStyle = "#000000";
-    // context.fillRect(0, 0, context.canvas.width, context.canvas.height);
-  });
+    context.fillStyle = "#000000";
+    context.fillRect(0, 0, context.canvas.width, context.canvas.height);
 
-  return <canvas width="600px" height="800px" ref={canvasRef} {...props} />;
+    contextRef.current = context;
+  }, []);
+
+  const startDrawing = ({ nativeEvent }: any) => {
+    const { offsetX, offsetY } = nativeEvent;
+    contextRef.current.beginPath();
+    contextRef.current.moveTo(offsetX, offsetY);
+    contextRef.current.lineTo(offsetX, offsetY);
+    contextRef.current.stroke();
+    setIsDrawing(true);
+    nativeEvent.preventDefault();
+  };
+
+  const draw = ({ nativeEvent }) => {
+    if (!isDrawing) {
+      return;
+    }
+
+    const { offsetX, offsetY } = nativeEvent;
+    contextRef.current.lineTo(offsetX, offsetY);
+    contextRef.current.stroke();
+    nativeEvent.preventDefault();
+  };
+
+  const stopDrawing = () => {
+    contextRef.current.closePath();
+    setIsDrawing(false);
+  };
+
+  const setToDraw = () => {
+    contextRef.current.globalCompositeOperation = "source-over";
+  };
+
+  const setToErase = () => {
+    contextRef.current.globalCompositeOperation = "destination-out";
+  };
+
+  return (
+    <canvas
+      id="canvas"
+      width="600px"
+      height="800px"
+      ref={canvasRef}
+      {...props}
+      onMouseDown={startDrawing}
+      onMouseMove={draw}
+      onMouseUp={stopDrawing}
+      onMouseLeave={stopDrawing}
+    />
+  );
 };
 
 export default Canvas;
